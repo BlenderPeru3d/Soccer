e17e17a30de086e2bff7d4c660e3a9290684047a
BlenderPeru3d <peru3dblender@gmail.com>
2020-10-02 20:02:24 -0500

    Base de datos añadida, no se ve.

---
+++ Soccer.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Soccer.Web.Data.Entities;
+
+namespace Soccer.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+
+        {
+
+        }
+
+        public DbSet<TeamEntity> Teams { get; set; }
+    }
+}
